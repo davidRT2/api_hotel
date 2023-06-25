c7805a445c82581c40aee5d0db9e26d9ed0e4a17
Akbardavid <akbardavid034@gmail.com>
2023-06-25 22:12:48 +0700

    routing api

--- src/routes.php
+++ src/routes.php
@@ -23,6 +23,19 @@ return function (App $app) {
         return $response->withJson(["status" => "success", "data" => $result], 200);
     });
 
+    $app->get("/api/v1/penginap/nama/{nama}", function (Request $request, Response $response, $args) {
+        $nama = $args['nama'];
+    
+        $sql = "SELECT * FROM penginap WHERE nama_penginap LIKE :nama";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':nama', '%' . $nama . '%');
+        $stmt->execute();
+        $result = $stmt->fetchAll();
+    
+        return $response->withJson(["status" => "success", "data" => $result], 200);
+    });
+    
+
     $app->get("/api/v1/penginap/{id}", function (Request $request, Response $response, $args) {
         $id = $args['id'];
 
@@ -84,6 +97,8 @@ return function (App $app) {
         return $response->withJson(["status" => "failed", "data" => "0"], 200);
     });
     
+    
+
     $app->post("/api/v1/penginap/", function (Request $request, Response $response) {
         $contentType = $request->getHeaderLine('Content-Type');
         $data = $request->getBody()->getContents();
@@ -109,41 +124,136 @@ return function (App $app) {
         }
     
         // Memeriksa apakah data yang dibutuhkan ada
-        $requiredFields = ['id_penginap', 'nama_penginap', 'id_kamar', 'durasi', 'check_in'];
+        $requiredFields = ['id_penginap', 'nama_penginap', 'id_kamar', 'durasi', 'check_in', 'telepon'];
         foreach ($requiredFields as $field) {
             if (!array_key_exists($field, $data)) {
                 return $response->withJson(["status" => "failed", "data" => "Missing required field: $field"], 400);
             }
         }
     
-        // Insert data ke database
-        $sql = "INSERT INTO penginap (id_penginap, nama_penginap, id_kamar, durasi, check_in) VALUES (:id_penginap, :nama_penginap, :id_kamar, :durasi, :check_in)";
+        // Validasi relasi ID kamar dengan tabel kamar
+        $id_kamar = $data['id_kamar'];
+        $sql = "SELECT COUNT(*) as count FROM kamar WHERE id_kamar = :id_kamar";
         $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':id_kamar', $id_kamar);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        if ($result['count'] == 0) {
+            return $response->withJson(["status" => "failed", "data" => "Invalid kamar ID"], 400);
+        }
     
-        $stmt->bindValue(':id_penginap', $data['id_penginap']);
-        $stmt->bindValue(':nama_penginap', $data['nama_penginap']);
-        $stmt->bindValue(':id_kamar', $data['id_kamar']);
-        $stmt->bindValue(':durasi', $data['durasi']);
-        $stmt->bindValue(':check_in', $data['check_in']);
+        // Insert data ke tabel penginap
+        $sqlInsert = "INSERT INTO penginap (id_penginap, nama_penginap, id_kamar, durasi, check_in, telepon) VALUES (:id_penginap, :nama_penginap, :id_kamar, :durasi, :check_in, :telepon)";
+        $stmtInsert = $this->db->prepare($sqlInsert);
     
-        if ($stmt->execute())
-            return $response->withJson(["status" => "success", "data" => "1"], 200);
+        $stmtInsert->bindValue(':id_penginap', $data['id_penginap']);
+        $stmtInsert->bindValue(':nama_penginap', $data['nama_penginap']);
+        $stmtInsert->bindValue(':id_kamar', $data['id_kamar']);
+        $stmtInsert->bindValue(':durasi', $data['durasi']);
+        $stmtInsert->bindValue(':check_in', $data['check_in']);
+        $stmtInsert->bindValue(':telepon', $data['telepon']);
+    
+        // Update kolom id_penginap pada tabel kamar
+        $sqlUpdate = "UPDATE kamar SET id_penginap = :id_penginap WHERE id_kamar = :id_kamar";
+        $stmtUpdate = $this->db->prepare($sqlUpdate);
+    
+        $stmtUpdate->bindValue(':id_penginap', $data['id_penginap']);
+        $stmtUpdate->bindValue(':id_kamar', $data['id_kamar']);
+    
+        try {
+            $this->db->beginTransaction();
+    
+            if ($stmtInsert->execute() && $stmtUpdate->execute()) {
+                $this->db->commit();
+                return $response->withJson(["status" => "success", "data" => "1"], 200);
+            }
+        } catch (PDOException $e) {
+            $this->db->rollBack();
+            return $response->withJson(["status" => "failed", "data" => "Error executing SQL statement: " . $e->getMessage()], 500);
+        }
     
         return $response->withJson(["status" => "failed", "data" => "0"], 200);
     });
-
+    
+    
+    $app->get("/api/v1/kamar/{id_kamar}", function (Request $request, Response $response, $args) {
+        $id_kamar = $args['id_kamar'];
+        
+        // Retrieve data from kamar table
+        $sql = "SELECT k.id_kamar, k.id_tipe, t.nama_tipe, t.harga_per_malam
+                FROM kamar k
+                JOIN tipe t ON k.id_tipe = t.id_tipe
+                WHERE k.id_kamar = :id_kamar";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':id_kamar', $id_kamar);
+        $stmt->execute();
+        $result = $stmt->fetch();
+        
+        if (!$result) {
+            return $response->withJson(["status" => "failed", "data" => "Kamar not found"], 404);
+        }
+        
+        $id_tipe = $result['id_tipe'];
+        
+        // Retrieve list of fasilitas for the given tipe
+        $sql = "SELECT f.id_fasilitas, f.nama_fasilitas
+                FROM fasilitas_tipe ft
+                JOIN fasilitas f ON ft.id_fasilitas = f.id_fasilitas
+                WHERE ft.id_tipe = :id_tipe";
+        $stmt = $this->db->prepare($sql);
+        $stmt->bindValue(':id_tipe', $id_tipe);
+        $stmt->execute();
+        $fasilitas = $stmt->fetchAll();
+        
+        $responsePayload = [
+            "id_kamar" => $result['id_kamar'],
+            "nama_tipe" => $result['nama_tipe'],
+            "harga_per_malam" => $result['harga_per_malam'],
+            "daftar_fasilitas" => $fasilitas
+        ];
+        
+        return $response->withJson(["status" => "success", "data" => $responsePayload], 200);
+    });
+    
     $app->delete("/api/v1/penginap/{id}", function (Request $request, Response $response, $args) {
         $id = $args['id'];
-
-        $sql = "DELETE FROM penginap WHERE id_penginap = :id";
-        $stmt = $this->db->prepare($sql);
-        $data = [
-            ":id" => $id
-        ];
-
-        if ($stmt->execute($data))
-            return $response->withJson(["status" => "success", "data" => "1"], 200);
-
+    
+        // Get the id_kamar associated with the penginap to be deleted
+        $sqlSelect = "SELECT id_kamar FROM penginap WHERE id_penginap = :id";
+        $stmtSelect = $this->db->prepare($sqlSelect);
+        $stmtSelect->bindValue(':id', $id);
+        $stmtSelect->execute();
+        $result = $stmtSelect->fetch();
+    
+        if (!$result) {
+            return $response->withJson(["status" => "failed", "data" => "Penginap not found"], 404);
+        }
+    
+        $id_kamar = $result['id_kamar'];
+    
+        // Delete the penginap from penginap table
+        $sqlDelete = "DELETE FROM penginap WHERE id_penginap = :id";
+        $stmtDelete = $this->db->prepare($sqlDelete);
+        $stmtDelete->bindValue(':id', $id);
+        
+        // Update id_penginap column in kamar table to NULL for the associated id_kamar
+        $sqlUpdate = "UPDATE kamar SET id_penginap = NULL WHERE id_kamar = :id_kamar";
+        $stmtUpdate = $this->db->prepare($sqlUpdate);
+        $stmtUpdate->bindValue(':id_kamar', $id_kamar);
+    
+        try {
+            $this->db->beginTransaction();
+    
+            if ($stmtDelete->execute() && $stmtUpdate->execute()) {
+                $this->db->commit();
+                return $response->withJson(["status" => "success", "data" => "1"], 200);
+            }
+        } catch (PDOException $e) {
+            $this->db->rollBack();
+            return $response->withJson(["status" => "failed", "data" => "Error executing SQL statement: " . $e->getMessage()], 500);
+        }
+    
         return $response->withJson(["status" => "failed", "data" => "0"], 200);
     });
+    
 };
